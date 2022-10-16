d310bb4b2158068bc42d311265985398ccb96c6e
Sandeep Soni <sandeepsonisrs01@gmail.com>
2022-10-16 18:40:16 +0530

    cors and allow access added

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -14,8 +14,12 @@ export class AuthInterceptor implements HttpInterceptor {
     
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         if(this.authGuard.authToken != null){
-            const modifiedReq = req.clone({
+            var modifiedReq = req.clone({
                 headers: req.headers.append('x-auth-token', this.authGuard.authToken)
+            });
+
+            modifiedReq = req.clone({
+                headers: req.headers.append('Access-Control-Allow-Origin', '*')
             });
             
             return next.handle(modifiedReq).pipe(
